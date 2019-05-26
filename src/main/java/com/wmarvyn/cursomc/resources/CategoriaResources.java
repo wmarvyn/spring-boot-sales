7a2f9097c229fbe0f73e0e03ac2d2d3c2f6d5129
Willian Marvyn <wmarvyn@gmail.com>
2019-05-25 21:12:27 -0300

    testando classe de cominio

--- src/main/java/com/wmarvyn/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/wmarvyn/cursomc/resources/CategoriaResources.java
@@ -1,18 +1,27 @@
 package com.wmarvyn.cursomc.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wmarvyn.cursomc.domain.Categoria;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando!";
+	@RequestMapping(method = RequestMethod.GET)
+	public ArrayList<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		ArrayList<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
-	
-	
+
 }
