86ab2aa4cff981fc40f837c58f051e2db9d03f65
Willian Marvyn <wmarvyn@gmail.com>
2019-11-04 07:29:11 -0300

    Iniciando o pagamento

---
+++ src/main/java/com/wmarvyn/cursomc/domain/Pagamento.java
@@ -0,0 +1,13 @@
+package com.wmarvyn.cursomc.domain;
+
+import com.wmarvyn.cursomc.domain.enums.EstadoPagamento;
+
+public class Pagamento {
+	
+	private Integer id;
+	private EstadoPagamento estado;
+	
+	
+	
+
+}
---
+++ src/main/java/com/wmarvyn/cursomc/domain/Pedido.java
@@ -0,0 +1,10 @@
+package com.wmarvyn.cursomc.domain;
+
+import java.util.Date;
+
+public class Pedido {
+
+	private Integer id;
+	private Date Instante;
+	
+}
