f404fb4509984e62801bc7e3191c5f50daf27297
Willian Marvyn <wmarvyn@gmail.com>
2019-11-15 22:19:06 -0300

    teste

--- src/main/java/com/wmarvyn/cursomc/repositores/PagamentoRepository.java
+++ src/main/java/com/wmarvyn/cursomc/repositores/PagamentoRepository.java
@@ -1,8 +1,6 @@
 package com.wmarvyn.cursomc.repositores;
 
 import com.wmarvyn.cursomc.domain.Pagamento;
-import com.wmarvyn.cursomc.domain.Categoria;
-;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
