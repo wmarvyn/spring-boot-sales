6334bfc1d7a33c58ebe88286ef53a6404cf9f977
Willian Marvyn <wmarvyn@gmail.com>
2020-01-09 21:37:30 -0300

    correcao de referncia ciclica

--- src/main/java/com/wmarvyn/cursomc/domain/ItemPedido.java
+++ src/main/java/com/wmarvyn/cursomc/domain/ItemPedido.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class ItemPedido implements Serializable {
@@ -29,6 +31,7 @@ public class ItemPedido implements Serializable {
         this.preco = preco;
     }
 
+    @JsonIgnore
     public Pedido getPedido(){
         return id.getPedido();
     }
